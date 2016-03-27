83da876fc98077311a5e23dabdb8961021e32764
Sehny <sehnoutka.martin@gmail.com>
2016-03-27 19:43:48 +0200

    automated tests - more parameters

--- gekon/automated_tests.cpp
+++ gekon/automated_tests.cpp
@@ -23,17 +23,29 @@ static const std::map<std::string, gekon::selection_fcn_t> selection_map = {
         {"tournament", gekon::s_tournament}
 };
 
+static const std::map<std::string, gekon::crossover_fcn_t> crossover_map = {
+        {"simple", gekon::c_simle},
+        {"convex", gekon::c_convex},
+        {"blx_a", gekon::c_blx_a}
+};
+
+static const std::map<std::string, gekon::mutation_fcn_t> mutation_map = {
+        {"swap", gekon::m_swap},
+        {"dynamic", gekon::m_dynamic}
+};
+
 class test_definition{
 public:
     gekon::Worker worker;
     std::string name, selection, crossover, mutation;
-    unsigned int number_of_threads, kernel_size, generation_size;
+    unsigned int number_of_threads, kernel_size, generation_size, max_iterations;
 
     test_definition() {
         name = selection = crossover = mutation = "default";
         number_of_threads = 4;
         kernel_size = 3;
-        generation_size = 500;
+        generation_size = 80;
+        max_iterations = 500;
     }
 };
 
@@ -46,7 +58,7 @@ int main(int argc, char **argv)
 
     std::shared_ptr<cpptoml::table> test_file = cpptoml::parse_file(argv[1]);
     std::vector<std::shared_ptr<cpptoml::table>> test_cases;
-    std::vector<struct test_definition> workers;
+    std::vector<test_definition> workers;
 
     try
     {
@@ -54,23 +66,34 @@ int main(int argc, char **argv)
         while (it != test_file->end()) {
             if (test_file->get(it->first)->is_table()) {
                 test_cases.push_back(test_file->get_table(it->first));
-                struct test_definition new_test;
+                test_definition new_test;
                 new_test.name = it->first;
                 workers.push_back(new_test);
             }
             ++it;
         }
-        //for (auto it_table : test_cases) {
         auto it_table = test_cases.begin();
         auto it_worker = workers.begin();
         while (it_table != test_cases.end() && it_worker != workers.end())
         {
-            //std::cout << "# Test definition:" << std::endl;
             if ((*it_table)->contains("threads")) {
                 const int64_t threads = (*it_table)->get("threads")->as<int64_t>()->get();
                 it_worker->number_of_threads = (unsigned int)threads;
                 it_worker->worker.overrideNumOfThreads((unsigned int)threads);
-                //std::cout << "Number of threads: " << threads << std::endl;
+            }
+            if ((*it_table)->contains("kernel_size")) {
+                const int64_t kernel_size = (*it_table)->get("kernel_size")->as<int64_t>()->get();
+                it_worker->kernel_size = (unsigned int)kernel_size;
+                it_worker->worker.setKernelSize((unsigned int)kernel_size);
+            }
+            if ((*it_table)->contains("generation_size")) {
+                const int64_t generation_size = (*it_table)->get("generation_size")->as<int64_t>()->get();
+                it_worker->generation_size = (unsigned int)generation_size;
+                it_worker->worker.setGenSize((unsigned int)generation_size);
+            }
+            if ((*it_table)->contains("max_iterations")) {
+                const int64_t max_iterations = (*it_table)->get("max_iterations")->as<int64_t>()->get();
+                it_worker->max_iterations = (unsigned int)max_iterations;
             }
             if ((*it_table)->contains("selection")) {
                 const std::string selection = (*it_table)->get("selection")->as<std::string>()->get();
@@ -81,15 +104,26 @@ int main(int argc, char **argv)
                 } else {
                     cerr << "Selection type not found!" << endl;
                 }
-                //std::cout << "Selection algorithm: " << selection << std::endl;
             }
             if ((*it_table)->contains("crossover")) {
-                const std::string crossover = (*it_table)->get("crossove")->as<std::string>()->get();
-                //std::cout << "Crossover algorithm: " << crossover<< std::endl;
+                const std::string crossover = (*it_table)->get("crossover")->as<std::string>()->get();
+                it_worker->crossover = crossover;
+                auto search = crossover_map.find(crossover);
+                if (search != crossover_map.end()) {
+                    it_worker->worker.setCrossoverFcn(search->second);
+                } else {
+                    cerr << "Crossover type not found!" << endl;
+                }
             }
             if ((*it_table)->contains("mutation")) {
                 const std::string mutation = (*it_table)->get("mutation")->as<std::string>()->get();
-                //std::cout << "Mutation algorithm: " << mutation << std::endl;
+                it_worker->mutation = mutation;
+                auto search = mutation_map.find(mutation);
+                if (search != mutation_map.end()) {
+                    it_worker->worker.setMutationFcn(search->second);
+                } else {
+                    cerr << "Mutation type not found!" << endl;
+                }
             }
             it_table++; it_worker++;
         }
@@ -128,19 +162,28 @@ int main(int argc, char **argv)
         test_output("Test name: ", it_worker->name);
         test_output("Number of threads: ", std::to_string(it_worker->number_of_threads));
         test_output("Selection type: ", it_worker->selection);
+        test_output("Crossover type: ", it_worker->crossover);
+        test_output("Mutation type: ", it_worker->mutation);
+        test_output("Kernel size: ", std::to_string(it_worker->kernel_size));
+        test_output("Generation size: ", std::to_string(it_worker->generation_size));
         cout << test_tag << "Run!" << endl;
 
         it_worker->worker.setTrSample(sample);
-        it_worker->worker.setGenSize(it_worker->generation_size);
 
         time_t start, end;
         time(&start);
-        int loops = 100;
-        for (int j = 0; j < loops; ++j) {
+        unsigned int loops = it_worker->max_iterations;
+        std::vector<double> elite;
+        elite.resize(loops);
+        for (unsigned int j = 0; j < loops; ++j) {
             auto ret = it_worker->worker.run();
-            cout << endl << "Elite: " << ret << endl
-            << "Iterations: " << j << "/" << loops << endl << endl;
+            elite.push_back(ret);
         }
+        cout << test_tag << "Elite: [";
+        for (unsigned int i = 0; i < elite.size() - 1; ++i) {
+            cout << elite[i] << ", ";
+        }
+        cout << elite[elite.size()-1]  << "]" << endl;
 
         time(&end);
         cout << test_tag << "Time elapsed: " << difftime(end, start) << endl;
--- gekon/gk_operators.h
+++ gekon/gk_operators.h
@@ -10,28 +10,28 @@
 
 namespace gekon
 {
-    
-	//float random(float min, float max);
-	ker_num_t random(ker_num_t min, ker_num_t max);
-    
+
+    //float random(float min, float max);
+    ker_num_t random(ker_num_t min, ker_num_t max);
+
     //crossover and mutation
     typedef std::function<std::vector<candidate_t>(candidate_t, candidate_t)> crossover_fcn_t;
     typedef std::function<void(candidate_t&, unsigned int, unsigned int)> mutation_fcn_t;
 
-	// Parameters
-	const float c_blx_a_alpha = 0.5; // Empiric paramter to widen range	
-	const float m_swap_prob = 0.1; // Probability of swap mutation
+    // Parameters
+    const float c_blx_a_alpha = 0.5; // Empiric paramter to widen range	
+    const float m_swap_prob = 0.1; // Probability of swap mutation
     const float m_dynamic_B = 5;
 
-	// crossovers
-	// BLX-a and BLX-a-b source http://www.tomaszgwiazda.com/blendX.htm
-	std::vector<candidate_t> c_blx_a(candidate_t X, candidate_t Y);
+    // crossovers
+    // BLX-a and BLX-a-b source http://www.tomaszgwiazda.com/blendX.htm
+    std::vector<candidate_t> c_blx_a(candidate_t X, candidate_t Y);
 
-	std::vector<candidate_t> c_simle(candidate_t X, candidate_t Y);
+    std::vector<candidate_t> c_simle(candidate_t X, candidate_t Y);
 
-	std::vector<candidate_t> c_convex(candidate_t X, candidate_t Y);
+    std::vector<candidate_t> c_convex(candidate_t X, candidate_t Y);
 
-	// mutations
+    // mutations
     void m_swap(candidate_t &X, unsigned int t, unsigned int T);
     void m_dynamic(candidate_t &X, unsigned int t, unsigned int T);
 }
