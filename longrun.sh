64d32ce8720e44fa623e32c835cc01335e6a4bfd
Vojtech Vladyka <vojtech.vladyka@gmail.com>
2016-04-19 19:45:37 +0200

    Updated tests

--- longrun.sh
+++ longrun.sh
@@ -4,9 +4,12 @@ for i in {1..6}
 do
 	echo "Batch for example$i.png"
 	echo "tournament-longrun.toml"
-	./gekon/automated_tests tournament-longrun.toml "samples_offic/example${i}.png" "samples_offic/example${i}_E.png" >> output/example$i.log
+	./gekon/automated_tests tournament-longrun.toml "samples_offic/example${i}.png" "samples_offic/example${i}_E.png" | tee -a output/example$i.log
+    cp output/* ~/Knock\ Knock\ Cloud/GeKon
 	echo "roulette-longrun.toml"
-	./gekon/automated_tests roulette-longrun.toml "samples_offic/example${i}.png" "samples_offic/example${i}_E.png" >> output/example$i.log
+	./gekon/automated_tests roulette-longrun.toml "samples_offic/example${i}.png" "samples_offic/example${i}_E.png" | tee -a output/example$i.log
+    cp output/* ~/Knock\ Knock\ Cloud/GeKon
 	echo "ranksel-longrun.toml"
-	./gekon/automated_tests ranksel-longrun.toml "samples_offic/example${i}.png" "samples_offic/example${i}_E.png" >> output/example$i.log
+	./gekon/automated_tests ranksel-longrun.toml "samples_offic/example${i}.png" "samples_offic/example${i}_E.png" | tee -a output/example$i.log
+    cp output/* ~/Knock\ Knock\ Cloud/GeKon
 done
